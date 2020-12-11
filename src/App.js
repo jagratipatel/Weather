6a8b78468bfbc333e043412826fc37e89f4a48b6
Jagrati <pateljagrati18@gmail.com>
2020-12-12 01:31:18 +0530

    component structure changed

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-class App extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      response:[]
-    };
-  }
-
-  componentDidMount() {
-    const q = 'Bangalore';
-    fetch(`http://api.weatherapi.com/v1/current.json?key=35bc0c3bf2ec4e4f905135251201012&q=${q}`)
-    .then(response => {
-      response.json();
-    })    
-.then(response => {
-	this.setState({
-          response : response
-  })
-})
-.catch(err => {
-	console.error(err);
-});
+function App() {
+    return (<div className="App"><h1>TODAY</h1></div>)
 }
 
-  render (){
-    console.log(this.state.response)
-    return(
-      <div className="App">
-        <span>City</span> 
-        <input type="text"/>
-      </div>)
-}
-}
-
-export default App; 
+export default App
